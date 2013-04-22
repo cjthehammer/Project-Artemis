ff753c04261e10e2996599e5480f44a6d4ed0bf5
thepackett <thepackett727@aol.com>
2013-04-21 21:33:06 -0500

    Set up project for netbeans

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: thepackett
+ *
+ * Created on April 21, 2013, 9:26 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
